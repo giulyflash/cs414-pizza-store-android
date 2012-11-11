e54a1f564daba3a5337be199089ec2c651472406
Justin Wilds <Justin Wilds@localhost>
2012-11-10 19:58:51 -0700

    Link "New User" button to new screen

--- src/pizzastore/android/PizzaKiosk.java
+++ src/pizzastore/android/PizzaKiosk.java
@@ -2,7 +2,11 @@ package pizzastore.android;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class PizzaKiosk extends Activity {
 
@@ -10,7 +14,16 @@ public class PizzaKiosk extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        super.setTitle("Pizza Kiosk");
+        
+        //TODO: figure out how to move this to onclick method for button
+        final Button button = (Button) findViewById(R.id.button1);
+        final Context context = this;
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Intent intent = new Intent(context, NewUser.class);
+                startActivity(intent); 
+            }
+        });
     }
 
     @Override
@@ -18,4 +31,7 @@ public class PizzaKiosk extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+
+
 }
